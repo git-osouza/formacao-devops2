8eb8da28a222f976f73ba4158b7f772be9f88a06
Ederson Guilherme de Souza <70489050+git-osouza@users.noreply.github.com>
2023-04-19 01:57:05 -0300

    Commit inicial

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fcf52bcf5db7b003"
+  instance_type = "t2.micro"
+  key_name = "iac-oregon"
+  tags = {
+    Name = "Primeira instancia com AWS"
+  }
+}
+
+
